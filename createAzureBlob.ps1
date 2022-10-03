c4545fec13b147bb8a866192700eac4b7f2a1b8a
Oscar Acevedo <80694876+acevos01@users.noreply.github.com>
2022-10-03 08:42:58 -0400

    uploading vars to azure with powershell

---
+++ createAzureBlob.ps1
@@ -0,0 +1,42 @@
+param(
+	[string]$storageAccountName,
+	[string]$storageContainerName,
+	[string]$storageAccountKey,
+	[string]$filename,
+	[string]$body
+)
+
+$method = "PUT"
+$headerDate = '2021-08-06'
+$headers = @{"x-ms-version"="$headerDate"}
+#$storageAccountName = "<your account name>"
+#$storageContainerName = "etl"
+#$storageAccountKey = "<your account key>"
+$Url = "https://$storageAccountName.blob.core.windows.net/$storageContainerName/$filename"
+#$body = "Hello world"
+$xmsdate = (get-date -format r).ToString()
+$headers.Add("x-ms-date",$xmsdate)
+$bytes = ([System.Text.Encoding]::UTF8.GetBytes($body))
+$contentLength = $bytes.length
+$headers.Add("Content-Length","$contentLength")
+$headers.Add("x-ms-blob-type","BlockBlob")
+
+$signatureString = "$method$([char]10)$([char]10)$([char]10)$contentLength$([char]10)$([char]10)$([char]10)$([char]10)$([char]10)$([char]10)$([char]10)$([char]10)$([char]10)"
+#Add CanonicalizedHeaders
+$signatureString += "x-ms-blob-type:" + $headers["x-ms-blob-type"] + "$([char]10)"
+$signatureString += "x-ms-date:" + $headers["x-ms-date"] + "$([char]10)"
+$signatureString += "x-ms-version:" + $headers["x-ms-version"] + "$([char]10)"
+#Add CanonicalizedResource
+$uri = New-Object System.Uri -ArgumentList $url
+$signatureString += "/" + $storageAccountName + $uri.AbsolutePath
+
+$dataToMac = [System.Text.Encoding]::UTF8.GetBytes($signatureString)
+
+$accountKeyBytes = [System.Convert]::FromBase64String($storageAccountKey)
+
+$hmac = new-object System.Security.Cryptography.HMACSHA256((,$accountKeyBytes))
+$signature = [System.Convert]::ToBase64String($hmac.ComputeHash($dataToMac))
+
+$headers.Add("Authorization", "SharedKey " + $storageAccountName + ":" + $signature);
+write-host -fore green $signatureString
+Invoke-RestMethod -Uri $Url -Method $method -headers $headers -Body $body
